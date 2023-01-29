772472f86d179d94ac1b54f5a69fc8302d866a4b
Berrehal Ouala Eddine <berrehaloualaeddine@gmail.com>
2023-01-29 19:48:51 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -13,39 +13,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix("dz-ar")->group(function () {
 
-    Route::get('/', [
+Route::get('/', [
 
-        'uses' => 'ChekoutController@index',
-        'as' => 'checkout.index'
+    'uses' => 'ChekoutController@index',
+    'as' => 'checkout.index'
 
-    ]);
+]);
 
-    Route::get('/product', [
+Route::get('/product', [
 
-        'uses' => 'ChekoutController@product',
-        'as' => 'checkout.product'
+    'uses' => 'ChekoutController@product',
+    'as' => 'checkout.product'
 
-    ]);
+]);
 
 
-    /*********** steeper methods  */
+/*********** steeper methods  */
 
-    Route::post('/confirm', [
-        'uses' => 'ChekoutController@confirm',
-        'as' => 'checkout.confirm'
-    ]);
+Route::post('/confirm', [
+    'uses' => 'ChekoutController@confirm',
+    'as' => 'checkout.confirm'
+]);
 
-    Route::post('/order', [
-        'uses' => 'ChekoutController@order',
-        'as' => 'checkout.order'
-    ]);
+Route::post('/order', [
+    'uses' => 'ChekoutController@order',
+    'as' => 'checkout.order'
+]);
 
-    Route::post('/contact', [
-        'uses' => 'ChekoutController@contact',
-        'as' => 'contact'
-    ]);
-});
+Route::post('/contact', [
+    'uses' => 'ChekoutController@contact',
+    'as' => 'contact'
+]);
+
+
+
 
 
